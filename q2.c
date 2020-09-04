26c932fdc59db590eb469be068ec0fd7a4570434
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-04 21:10:41 +0530

    Just some minor changes on instruction of my dad

--- q2.c
+++ q2.c
@@ -4,11 +4,12 @@ int main(int argc, char const *argv[])
   printf("Enter distance between any two cities\n");
   int h;
   scanf("%d",&h);
-  float q,w,e,r;
+  float q,w,e,r,t;
   q=h*1000;
   w=q*100;
   e=w/2.54;
   r=e/12;
-  printf("distance between cities is %f meter or %f centimeter or %f inch or % f feet\n",q,w,e,r);
+  t=h/1.609;
+  printf("distance between cities is %f meter or %f centimeter or %f inch or %f feet or %f in miles.\n",q,w,e,r,t);
   return 0;
 }
