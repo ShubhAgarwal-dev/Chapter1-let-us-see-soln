f70409b0e737b05a755814c475e323b1371d45bc
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-04 18:37:05 +0530

    Made error in reading the question that created the error which i corrected now

--- q7.c
+++ q7.c
@@ -2,26 +2,26 @@
 int main(int argc, char const *argv[])
 {
   /* Paper Size Questions */
-  int q,w,e,r,t,y,u,i,o,p;
+  float q,w,e,r,t,y,u,i,o,p;
   q=1189;
   w=841;
-  e=q*2;
-  r=w*2;
-  t=e*2;
-  y=r*2;
-  u=t*2;
-  i=y*2;
-  o=u*2;
-  p=i*2;
+  e=q/2;
+  r=w/2;
+  t=e/2;
+  y=r/2;
+  u=t/2;
+  i=y/2;
+  o=u/2;
+  p=i/2;
   printf("All distances are given in mm\n");
-  printf("size of A0 sheet is %d by %d\n",q,w);
-  printf("size of A1 sheet is %d by %d\n",w,e);
-  printf("size of A2 sheet is %d by %d\n",e,r);
-  printf("size of A3 sheet is %d by %d\n",r,t);
-  printf("size of A4 sheet is %d by %d\n",t,y);
-  printf("size of A5 sheet is %d by %d\n",y,u);
-  printf("size of A6 sheet is %d by %d\n",u,i);
-  printf("size of A7 sheet is %d by %d\n",i,o);
-  printf("size of A8 sheet is %d by %d\n",o,p);
+  printf("size of A0 sheet is %f by %f\n",q,w);
+  printf("size of A1 sheet is %f by %f\n",w,e);
+  printf("size of A2 sheet is %f by %f\n",e,r);
+  printf("size of A3 sheet is %f by %f\n",r,t);
+  printf("size of A4 sheet is %f by %f\n",t,y);
+  printf("size of A5 sheet is %f by %f\n",y,u);
+  printf("size of A6 sheet is %f by %f\n",u,i);
+  printf("size of A7 sheet is %f by %f\n",i,o);
+  printf("size of A8 sheet is %f by %f\n",o,p);
   return 0;
 }
