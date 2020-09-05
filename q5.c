b430c8165c57a2dd8859325f1b8bb2d25b7d3da8
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-05 08:42:11 +0530

    Spelling error corrected

--- q5.c
+++ q5.c
@@ -3,13 +3,13 @@ int main(int argc, char const *argv[])
 {
   /*Author shubh*/
   float q,w,e,r,t,y,u;
-  printf("tell length,bredth of rectangle and tell rediuc of circle\n");
+  printf("tell length,bredth of rectangle and tell radius of circle\n");
   scanf("%f %f %f",&q,&w,&e );
   r=2*q +2*w;
   t=q*w;
   y=3.1416*2*e;
   u=3.1416*e*e;
-  printf("Perimeter of triangle is %f and its area is %f\n",r,t );
+  printf("Perimeter of rectangle is %f and its area is %f\n",r,t );
   printf("Circumference of circle of radius %f is %f and its area is %f\n",e,y,u );
   return 0;
 }
